e2bc1950eeaa5d01f0dff58172b91adcf4ee3733
Anh Le <lehoanganh.le2001@gmail.com>
2023-03-20 01:04:02 +0700

    added telescope fzf native

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -75,6 +75,13 @@ return {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
     version = false, -- telescope did only one release, so use HEAD for now
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
     keys = {
       { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
       { "<leader>sf", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
